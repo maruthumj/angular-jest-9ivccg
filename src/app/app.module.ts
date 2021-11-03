acbc7af8079a1366a45ce5994de51cc53a9832b4
maruthumj <maruthupandian72@gmail.com>
2021-11-03 11:53:26 +0530

    created a button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MasterComponent } from './master/master.component';
-
+import {button-component} from './button-component/button-component.component';
 @NgModule({
   imports: [
     BrowserModule,
@@ -14,7 +14,8 @@ import { MasterComponent } from './master/master.component';
   ],
   declarations: [
     AppComponent,
-    MasterComponent
+    MasterComponent,
+    button-component
   ],
   bootstrap: [
     AppComponent
